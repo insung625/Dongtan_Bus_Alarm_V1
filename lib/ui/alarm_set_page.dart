211ba4033aca7358710cdc4e6434c4bc07941d56
이인성 <inseong@iinseong-ui-MacBookPro.local>
2020-01-02 16:01:07 +0900

    Basic UI of alarm_set_page is changed

--- lib/ui/alarm_set_page.dart
+++ lib/ui/alarm_set_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:search_widget/search_widget.dart';
 
 class AlarmSetPage extends StatefulWidget {
   @override
@@ -87,7 +88,10 @@ class _AlarmBodyState extends State<AlarmBody> {
 
   bool _togglevalue = false;
   var _numMinutes = 5;
+  FocusNode _focusNodeStation = FocusNode();
+  FocusNode _focusNodeBus = FocusNode();
 
+  Color _mainColor = Colors.blueGrey;
 
   @override
   Widget build(BuildContext context) {
@@ -101,16 +105,29 @@ class _AlarmBodyState extends State<AlarmBody> {
         Padding(
            padding: const EdgeInsets.all(8),
            child: TextField(
+             focusNode: _focusNodeStation,
              onChanged: (value) {
 
              },
              controller: stationController,
              decoration: InputDecoration(
-               prefixIcon: Icon(Icons.search),
+               prefixIcon: Icon(Icons.search, color: _mainColor,),
                labelText: "정류장",
                hintText: "정류장명",
-               border: OutlineInputBorder(
-                 borderRadius: BorderRadius.all(Radius.circular(25)),
+               labelStyle: TextStyle(
+                  fontFamily: 'Godo',
+                  fontSize: 20,
+                  color: _mainColor
+               ),
+               enabledBorder: OutlineInputBorder(
+                 borderSide: BorderSide(
+                   color: _mainColor
+                 )
+               ),
+               focusedBorder: OutlineInputBorder(
+                 borderSide: BorderSide(
+                   color: _mainColor
+                 )
                )
              ),
            ),
@@ -124,12 +141,24 @@ class _AlarmBodyState extends State<AlarmBody> {
             },
             controller: busController,
             decoration: InputDecoration(
-              prefixIcon: Icon(Icons.search),
+              prefixIcon: Icon(Icons.search, color: _mainColor),
               labelText: "버스",
               hintText: "번호",
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(25)),
-              )
+              labelStyle: TextStyle(
+                  fontFamily: 'Godo',
+                  fontSize: 20,
+                  color: _mainColor
+               ),
+               enabledBorder: OutlineInputBorder(
+                 borderSide: BorderSide(
+                   color: _mainColor
+                 )
+               ),
+               focusedBorder: OutlineInputBorder(
+                 borderSide: BorderSide(
+                   color: _mainColor
+                 )
+               )
             ),
 
           ),
@@ -146,19 +175,12 @@ class _AlarmBodyState extends State<AlarmBody> {
                   style: TextStyle(
                     fontFamily: 'Godo',
                     fontSize: 20,
+                    color: _mainColor
                   ),
                 )
               ),
-              // Text(
-              //   '도착',
-              //   style: TextStyle(
-              //       fontFamily: 'Godo',
-              //       fontSize: 20,
-              //     ),
-
-              // ),
               IconButton(
-                icon: Icon(Icons.remove_circle_outline), 
+                icon: Icon(Icons.remove_circle_outline, color: _mainColor), 
                 onPressed: () {
                   setState(() {
                     _numMinutes--;
@@ -170,10 +192,11 @@ class _AlarmBodyState extends State<AlarmBody> {
                 style: TextStyle(
                     fontFamily: 'Godo',
                     fontSize: 20,
+                    color: _mainColor
                   ),
               ),
               IconButton(
-                icon: Icon(Icons.add_circle_outline), 
+                icon: Icon(Icons.add_circle_outline, color: _mainColor), 
                 onPressed: () {
                   setState(() {
                     _numMinutes++;
@@ -185,19 +208,65 @@ class _AlarmBodyState extends State<AlarmBody> {
                 style: TextStyle(
                     fontFamily: 'Godo',
                     fontSize: 20,
+                    color: _mainColor
                   ),
               ),
             ]
           ),
         ),
         //토글 스위치 이용한 진동/알람
-        AnimatedContainer(
-          duration: Duration(milliseconds: 500),
-          width: 100,
-          height: 50,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25),
-            color: _togglevalue ? Colors.teal : Colors.amber
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25.0),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Text(
+                  '진동',
+                  style: TextStyle(
+                        fontFamily: 'Godo',
+                        fontSize: 20,
+                        color: _mainColor
+                      ),
+                ),
+              ),
+              AnimatedContainer(
+                duration: Duration(milliseconds: 500),
+                width: 80,
+                height: 40,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  color: _togglevalue ? Colors.greenAccent[100].withOpacity(0.5) : Colors.grey.withOpacity(0.5),
+                ),
+                child: Stack(
+                  children: <Widget>[
+                    AnimatedPositioned(
+                      duration: Duration(milliseconds: 500),
+                      curve: Curves.easeIn,
+                      top: 0,
+                      left: _togglevalue ? 40 : 0,
+                      right: _togglevalue ? 0 : 40,
+                      child: InkWell( //inkwell 실제 버튼처럼 동작하게 해줌.
+                        onTap: (){
+                          setState(() {
+                            _togglevalue = !_togglevalue;
+                          });
+                        },
+                        child: AnimatedSwitcher(
+                           duration: Duration(milliseconds: 500),
+                           transitionBuilder: (Widget child, Animation<double> animation){
+                             return RotationTransition(child: child, turns: animation);
+                           },
+                           child: _togglevalue ? 
+                           Icon(Icons.check_circle, color: Colors.green, size: 40, key: UniqueKey()) : 
+                           Icon(Icons.remove_circle, color: Colors.grey, size: 40, key: UniqueKey()),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
 
